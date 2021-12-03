7a6cf78e273fe11d419793d3ef14cc0260ad8f76
Leon Linhart <themrmilchmann@gmail.com>
2021-12-03 12:49:17 +0100

    feat(day03): add part 2

--- src/main/kotlin/days/Day03.kt
+++ src/main/kotlin/days/Day03.kt
@@ -24,35 +24,66 @@ package days
 import utils.*
 
 fun main() {
-    data class BitsAtIndex(val index: Int, val gammaBit: Char, val epsilonBit: Char)
-    data class IncompleteDiagnosticData(val gammaBits: String, val epsilonBits: String)
+    fun part1(): Int {
+        data class DiagnosticReport(val gammaRate: Int, val epsilonRate: Int)
+        data class BitsAtIndex(val index: Int, val gammaBit: Char, val epsilonBit: Char)
+        data class IncompleteDiagnosticData(val gammaBits: String, val epsilonBits: String)
 
-    data class DiagnosticReport(val gammaRate: Int, val epsilonRate: Int)
+        val incompleteDiagnosticData = readInput()
+            .flatMap { sequence -> sequence.mapIndexed { index, c -> index to c } }
+            .groupBy(keySelector = { it.first }, valueTransform = { it.second })
+            .map { (index, values) ->
+                val bits = values.groupingBy { it }.eachCount()
 
-    val incompleteDiagnosticData = readInput()
-        .flatMap { sequence -> sequence.mapIndexed { index, c -> index to c } }
-        .groupBy { it.first }
-        .map { (index, values) ->
-            val bits = values.map { it.second }.groupingBy { it }.eachCount()
+                BitsAtIndex(
+                    index,
+                    bits.maxByOrNull { it.value }!!.key,
+                    bits.minByOrNull { it.value }!!.key
+                )
+            }
+            .sortedBy { it.index }
+            .fold(initial = IncompleteDiagnosticData(gammaBits = "", epsilonBits = "")) { acc, it ->
+                IncompleteDiagnosticData(
+                    gammaBits = acc.gammaBits + it.gammaBit,
+                    epsilonBits = acc.epsilonBits + it.epsilonBit
+                )
+            }
 
-            BitsAtIndex(
-                index,
-                bits.maxByOrNull { it.value }!!.key,
-                bits.minByOrNull { it.value }!!.key
-            )
+        val report = DiagnosticReport(
+            gammaRate = incompleteDiagnosticData.gammaBits.toInt(radix = 2),
+            epsilonRate = incompleteDiagnosticData.epsilonBits.toInt(radix = 2)
+        )
+
+        return report.gammaRate * report.epsilonRate
+    }
+
+    fun part2(): Int {
+        val input = readInput()
+
+        tailrec fun Pair<List<String>, List<String>>.filterDownToOne(index: Int = 0, max: Int): Pair<List<String>, List<String>> {
+            var (first, second) = this
+
+            if (first.size > 1) {
+                val occs = first.groupingBy { it[index] }.eachCount()
+                val mcb = if (occs['0']!! <= occs['1']!!) '1' else '0'
+
+                first = first.filter { it[index] == mcb }
+            }
+
+            if (second.size > 1) {
+                val occs = second.groupingBy { it[index] }.eachCount()
+                val lcb = if (occs['0']!! > occs['1']!!) '1' else '0'
+
+                second = second.filter { it[index] == lcb }
+            }
+
+            return if (index < max) (first to second).filterDownToOne(index + 1, max) else (first to second)
         }
-        .sortedBy { it.index }
-        .fold(initial = IncompleteDiagnosticData(gammaBits = "", epsilonBits = "")) { acc, it ->
-            IncompleteDiagnosticData(
-                gammaBits = acc.gammaBits + it.gammaBit,
-                epsilonBits = acc.epsilonBits + it.epsilonBit
-            )
-        }
 
-    val report = DiagnosticReport(
-        gammaRate = incompleteDiagnosticData.gammaBits.toInt(radix = 2),
-        epsilonRate = incompleteDiagnosticData.epsilonBits.toInt(radix = 2)
-    )
+        return (input to input).filterDownToOne(max = input.first().length - 1)
+            .let { it.first.first().toInt(radix = 2) * it.second.first().toInt(radix = 2) }
+    }
 
-    println("Part 1: ${report.gammaRate * report.epsilonRate}")
+    println("Part 1: ${part1()}")
+    println("Part 2: ${part2()}")
 }
