e688ab017d210fcf52f55ab98246ee5c08bf68fb
Leon Linhart <themrmilchmann@gmail.com>
2021-12-06 23:59:07 +0100

    feat(day05): add part 2

--- src/main/kotlin/days/Day05.kt
+++ src/main/kotlin/days/Day05.kt
@@ -22,6 +22,8 @@
 package days
 
 import utils.*
+import kotlin.math.absoluteValue
+import kotlin.math.sign
 
 fun main() {
     data class Point(val x: Int, val y: Int)
@@ -38,21 +40,43 @@ fun main() {
             )
         }
 
-    val grid = Array(segments.maxOf { maxOf(it.start.y, it.end.y) } + 1) {
-        IntArray( segments.maxOf { maxOf(it.start.x, it.end.x) } + 1)
-    }
+    fun part1(): Int {
+        val grid = Array(segments.maxOf { maxOf(it.start.y, it.end.y) } + 1) {
+            IntArray( segments.maxOf { maxOf(it.start.x, it.end.x) } + 1)
+        }
 
-    for (segment in segments) {
-        if (segment.start.x == segment.end.x) {
-            segment.start.diff(segment.end, Point::y).forEach { y ->
-                grid[y][segment.start.x]++
-            }
-        } else if (segment.start.y == segment.end.y) {
-            segment.start.diff(segment.end, Point::x).forEach { x ->
-                grid[segment.start.y][x]++
+        for (segment in segments) {
+            if (segment.start.x == segment.end.x) {
+                segment.start.diff(segment.end, Point::y).forEach { y ->
+                    grid[y][segment.start.x]++
+                }
+            } else if (segment.start.y == segment.end.y) {
+                segment.start.diff(segment.end, Point::x).forEach { x ->
+                    grid[segment.start.y][x]++
+                }
             }
         }
+
+        return grid.flatMap { it.filter { it > 1 } }.count()
     }
 
-    println("Part 1: ${grid.flatMap { it.filter { it > 1 } }.count()}")
+    fun part2(): Int {
+        val grid = Array(segments.maxOf { maxOf(it.start.y, it.end.y) } + 1) {
+            IntArray( segments.maxOf { maxOf(it.start.x, it.end.x) } + 1)
+        }
+
+        for (segment in segments) {
+            val dX = (segment.end.x - segment.start.x).sign
+            val dY = (segment.end.y - segment.start.y).sign
+
+            for (t in 0..maxOf((segment.start.x - segment.end.x).absoluteValue, (segment.start.y - segment.end.y).absoluteValue)) {
+                grid[segment.start.y + t * dY][segment.start.x + t * dX]++
+            }
+        }
+
+        return grid.flatMap { it.filter { it > 1 } }.count()
+    }
+
+    println("Part 1: ${part1()}")
+    println("Part 2: ${part2()}")
 }
