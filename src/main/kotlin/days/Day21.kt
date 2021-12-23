fe0e3b36dbcb486c24f3ed023827f28b0931febe
Leon Linhart <themrmilchmann@gmail.com>
2021-12-23 17:37:40 +0100

    feat(day21): add part 2

--- src/main/kotlin/days/Day21.kt
+++ src/main/kotlin/days/Day21.kt
@@ -29,22 +29,78 @@ fun main() {
     val players = readInput().map { line ->
         val (_, pos) =  pattern.matchEntire(line)?.destructured ?: error("Could not parse $line")
         pos.toInt()
-    }.toMutableList()
-
-    val scores = players.map { 0 }.toMutableList()
-    var roll = 0
-
-    outer@while (true) {
-        for (player in players.indices) {
-            var advance = 0
-            for (i in 0..2) advance += ((++roll - 1) % 100) + 1
-
-            players[player] = ((players[player] + advance - 1) % 10) + 1
-            scores[player] = scores[player] + players[player]
-
-            if (scores[player] >= 1000) break@outer
-        }
     }
 
-    println("Part 1: ${scores.minOf { it } * roll}")
+    fun part1(): Int {
+        val positions = players.mapTo(mutableListOf()) { it }
+        val scores = players.mapTo(mutableListOf()) { 0 }
+
+        var roll = 0
+
+        outer@while (true) {
+            for (player in players.indices) {
+                var advance = 0
+                for (i in 0..2) advance += ((++roll - 1) % 100) + 1
+
+                positions[player] = ((positions[player] + advance - 1) % 10) + 1
+                scores[player] = scores[player] + positions[player]
+
+                if (scores[player] >= 1000) break@outer
+            }
+        }
+
+        return scores.minOf { it } * roll
+    }
+
+    fun part2(): Long {
+        data class State(
+            val curPos: Int, val curScore: Int,
+            val nxtPos: Int, val nxtScore: Int,
+            val roll: Int
+        )
+
+        data class Result(val p1Wins: Long, val p2Wins: Long)
+
+        val rolls = listOf(1, 2, 3).let { sides ->
+            sides.flatMap { first -> sides.flatMap { second -> sides.map { third -> first + second + third } } }
+        }
+
+        val cache = mutableMapOf<State, Result>()
+
+        fun State.process(): Result = when {
+            curScore >= 21 || nxtScore >= 21 -> {
+                if (roll % 2 == 0 && curScore <= nxtScore)
+                    Result(p1Wins = 1, p2Wins = 0)
+                else
+                    Result(p1Wins = 0, p2Wins = 1)
+            }
+            else -> cache.getOrPut(this) {
+                rolls.map { advance ->
+                    val newPos = ((curPos + advance - 1) % 10) + 1
+
+                    State(
+                        curPos = nxtPos,
+                        curScore = nxtScore,
+                        nxtPos = newPos,
+                        nxtScore = curScore + newPos,
+                        roll = roll + 1
+                    )
+                }.map(State::process)
+                    .reduce { acc, e ->
+                        Result(acc.p1Wins + e.p1Wins, acc.p2Wins + e.p2Wins)
+                    }
+            }
+        }
+
+        return State(
+            curPos = players[0],
+            curScore = 0,
+            nxtPos = players[1],
+            nxtScore = 0,
+            roll = 0
+        ).process().let { maxOf(it.p1Wins, it.p2Wins) }
+    }
+
+    println("Part 1: ${part1()}")
+    println("Part 2: ${part2()}")
 }
